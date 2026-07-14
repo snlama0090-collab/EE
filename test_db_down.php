5915487f56ccc402a17aed6104092dfa7312cdaa
Nis <snlama0090@gmail.com>
2026-07-14 08:57:44 +0000

    Jul 14, 2026, 2:42 PM

--- test_db_down.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once 'app/config/config.php';
-
-// This will trigger getDB() → Database::connect() → PDO exception → our new catch block
-$db = Database::getInstance();
-$db->connect();

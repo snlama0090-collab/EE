dd904dd6ee23c99f9850e7b072336006966626bd
Nis <snlama0090@gmail.com>
2026-07-19 12:20:53 +0000

    Jul 19, 2026, 6:05 PM

--- public/dashboard/sections/bookings.php
+++ public/dashboard/sections/bookings.php
@@ -141,13 +141,3 @@ $bookings = $stmt->fetchAll();
     </div>
 <?php endif; ?>
 </write_to_file>
-<task_progress>
-- [x] Read all listing section files
-- [x] Add modern listing CSS to dashboard.css
-- [x] Refactor admin_sections/users.php
-- [x] Refactor admin_sections/stations.php
-- [x] Refactor admin_sections/reviews.php
-- [x] Refactor admin_sections/overview.php tables
-- [x] Refactor sections/bookings.php
-</task_progress>
-</write_to_file>
